9dce6cafaa9fef70a78ed5b4a63bb54689efc256
KlinVid <stas.nik12@gmail.com>
2023-10-17 17:57:05 +0300

    add post in page.php

--- page.php
+++ page.php
@@ -15,75 +15,37 @@
     <section class="page blog">
         <div class="container">
             <div class="acticle">
-                <article class="article__item">
-                    <div>
-                        <div class="article__thumb">
-                            <img src="<?php echo PODKAST_IMG_DIR ?>/thumb/cover-1.png" alt="">
-                        </div>
-                        <a href="" class="article__category">podcast</a>
-                        <h3 class="article__title"><a href="post.html">Setup your own podcast</a></h3>
-                        <p class="article__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                    <div class="article__info">
-                        <div class="article__tags">
-                            <a href="">art</a>
-                            <a href="">tips and trick</a>
-                            <a href="">creative</a>
-                        </div>
-                        <span class="article__date">Sep 14, 2021</span>
-                    </div>
-                </article>
-                <article class="article__item">
-                    <div>
-                        <div class="article__thumb">
-                            <img src="<?php echo PODKAST_IMG_DIR ?>/thumb/cover-2.png" alt="">
-                        </div>
-                        <a href="" class="article__category">podcast</a>
-                        <h3 class="article__title"><a href="post.html">Doodle Artwork 101</a></h3>
-                        <p class="article__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                    <div class="article__info">
-                        <div class="article__tags">
-                            <a href="">business</a>
-                            <a href="">startup</a>
-                        </div>
-                        <span class="article__date">Sep 14, 2021</span>
-                    </div>
-                </article>
-                <article class="article__item">
-                    <div>
-                        <div class="article__thumb">
-                            <img src="<?php echo PODKAST_IMG_DIR ?>/thumb/cover-3.png" alt="">
-                        </div>
-                        <a href="" class="article__category">podcast</a>
-                        <h3 class="article__title"><a href="post.html">Mother Nature Taking Over</a></h3>
-                        <p class="article__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                    <div class="article__info">
-                        <div class="article__tags">
-                            <a href="">news</a>
-                            <a href="">nature</a>
-                        </div>
-                        <span class="article__date">Sep 14, 2021</span>
-                    </div>
-                </article>
-                <article class="article__item">
-                    <div>
-                        <div class="article__thumb">
-                            <img src="<?php echo PODKAST_IMG_DIR ?>/thumb/cover-4.png" alt="post.html">
-                        </div>
-                        <a href="" class="article__category">podcast</a>
-                        <h3 class="article__title"><a href="">How to Be Productive</a></h3>
-                        <p class="article__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                    <div class="article__info">
-                        <div class="article__tags">
-                            <a href="">productivity</a>
-                            <a href="">mindset</a>
-                        </div>
-                        <span class="article__date">Sep 14, 2021</span>
-                    </div>
-                </article>
+                <?php
+                    $args = array(
+                        'numberposts' => 4,
+                    );
+
+                    $result = wp_get_recent_posts( $args );
+
+                    foreach( $result as $p ){
+                        $thumbnail = get_the_post_thumbnail( $p['ID'], 'full' );
+                        ?>
+                        <article class="article__item">
+                            <div>
+                                <div class="article__thumb">
+                                    <?php echo $thumbnail;?>
+                                </div>
+                                <a href="" class="article__category">podcast</a>
+                                <h3 class="article__title"><a href="<?php echo get_permalink($p['ID']) ?>"><?php echo $p['post_title'] ?></a></h3>
+                                <?php echo substr($p['post_content'], 0, 230 )?>
+                            </div>
+                            <div class="article__info">
+                                <div class="article__tags">
+                                    <a href="">art</a>
+                                    <a href="">tips and trick</a>
+                                    <a href="">creative</a>
+                                </div>
+                                <span class="article__date"><?php $date = date_i18n('M j, Y', strtotime($p['post_date'])); echo $date; ?></span>
+                            </div>
+                        </article>
+                        <?php
+                    }
+                ?>
             </div>
             <a href="blog.html" class="btn-primary">Load More</a>
         </div>
