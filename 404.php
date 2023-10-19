99f79bf64ee27ae563d42cfb168a4f9357de1962
KlinVid <stas.nik12@gmail.com>
2023-10-19 19:11:55 +0300

    add 404.php

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+        <div class="container">
+            <div class="main">
+                <h1 class="main__title">404 <br><span>ERROR</span></h1>
+                <p class="main__description">We are sorry, but the page you requested was not found</p>
+            </div>
+        </div>
+        <?php get_sidebar(); ?>
+    </main>
+
+<?php get_footer() ?>
